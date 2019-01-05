6d88d05bb068b0060ec798807ad9fccef75500bb
RyusukeMarumoto <marumoryusu@gmail.com>
2019-01-05 09:36:53 +0000

    before log_in method

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+
+  # 静的ページ
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  
+  # ユーザーページ
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   resources :users
+  
+  # セッション
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
